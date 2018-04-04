921309c86cd0d77f55e17a435fa2b8ba2ab6715d
JackAZ <13146653740@163.com>
2018-04-04 17:51:37 +0800

    add images and pythonfile

---
+++ cvtetsphone.py
@@ -0,0 +1,145 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import cv2
+import numpy as np
+from config import Config
+
+def random_colors(N):
+    np.random.seed(1)
+    colors = [tuple(255 * np.random.rand(3)) for _ in range(N)]
+    return colors
+
+
+def apply_mask(image, mask, color, alpha=0.5):
+    """apply mask to image"""
+    for n, c in enumerate(color):
+        image[:, :, n] = np.where(
+            mask == 1,
+            image[:, :, n] * (1 - alpha) + alpha * c,
+            image[:, :, n]
+        )
+    return image
+
+
+def display_instances(image, boxes, masks, ids, names, scores):
+    """
+        take the image and results and apply the mask, box, and Label
+    """
+    n_instances = boxes.shape[0]
+    colors = random_colors(n_instances)
+
+    if not n_instances:
+        print('NO INSTANCES TO DISPLAY')
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == ids.shape[0]
+
+    for i, color in enumerate(colors):
+        if not np.any(boxes[i]):
+            continue
+
+        y1, x1, y2, x2 = boxes[i]
+        label = names[ids[i]]
+
+        score = scores[i] if scores is not None else None
+        caption = '{} {:.2f}'.format(label, score) if score else label
+        mask = masks[:, :, i]
+
+        image = apply_mask(image, mask, color)
+        image = cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+        image = cv2.putText(
+        image, caption, (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.7, color, 2
+        )
+
+    return image
+
+
+if __name__ == '__main__':
+    """
+        test everything
+    """
+    import os
+    import sys
+    import coco
+    import utils
+    import model as modellib
+
+    ROOT_DIR = os.getcwd()
+    MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+    COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_cellphone.h5")
+
+
+    class ShapesConfig(Config):
+        """Configuration for training on the toy shapes dataset.
+        Derives from the base Config class and overrides values specific
+        to the toy shapes dataset.
+        """
+        # Give the configuration a recognizable name
+        NAME = "shapes"
+
+        # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+        # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
+
+        # Number of classes (including background)
+        NUM_CLASSES = 1 + 1  # background + 3 shapes
+
+        # Use small images for faster training. Set the limits of the small side
+        # the large side, and that determines the image shape.
+        IMAGE_MIN_DIM = 320
+        IMAGE_MAX_DIM = 512
+
+        # Use smaller anchors because our image and objects are small
+        RPN_ANCHOR_SCALES = (8 * 4, 16 * 4, 32 * 4, 64 * 4, 128 * 4)  # anchor side in pixels
+
+        # Reduce training ROIs per image because the images are small and have
+        # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+        TRAIN_ROIS_PER_IMAGE = 32
+
+        # Use a small epoch since the data is simple
+        STEPS_PER_EPOCH = 100
+
+        # use small validation steps since the epoch is small
+        VALIDATION_STEPS = 5
+
+    class InferenceConfig(ShapesConfig):
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
+
+    config = InferenceConfig()
+    config.display()
+
+    model = modellib.MaskRCNN(
+        mode="inference", model_dir=MODEL_DIR, config=config
+    )
+    model.load_weights(COCO_MODEL_PATH, by_name=True)
+    class_names = [
+        'BG', 'cellphone'
+    ]
+
+    capture = cv2.VideoCapture(0)
+
+    # these 2 lines can be removed if you dont have a 1080p camera.
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 800)
+
+    while True:
+        ret, frame = capture.read()
+        results = model.detect([frame], verbose=0)
+        r = results[0]
+        frame = display_instances(
+            frame, r['rois'], r['masks'], r['class_ids'], class_names, r['scores']
+        )
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+
+# In[ ]:
